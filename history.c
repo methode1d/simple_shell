e871132d98137dfb4da3c595950bf85d0516506f
Winebald BANITUZE <142157809+Winebald250@users.noreply.github.com>
2023-12-13 16:07:53 +0200

    Update history.c

--- history.c
+++ history.c
@@ -2,7 +2,7 @@
 
 /**
  * get_history_file - gets the history file
- * @info: parameter struct
+ * @info: parameter struct winebald
  *
  * Return: allocated string containg history file
  */
@@ -26,7 +26,7 @@ char *get_history_file(info_t *info)
 
 /**
  * write_history - creates a file, or appends to an existing file
- * @info: the parameter struct
+ * @info: the parameter struct banituze on the flow for real
  *
  * Return: 1 on success, else -1
  */
