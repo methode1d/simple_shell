ad1fd4c024e20b3d0664b8910cf74b3745d2e98c
Winebald BANITUZE <142157809+Winebald250@users.noreply.github.com>
2023-12-13 16:06:52 +0200

    Update getenv.c by winebald

--- getenv.c
+++ getenv.c
@@ -2,7 +2,7 @@
 
 /**
  * get_environ - returns the string array copy of our environ
- * @info: Structure containing potential arguments. Used to maintain
+ * @info: Structure containing potential arguments. Used to maintain winebald
  *          constant function prototype.
  * Return: Always 0
  */
@@ -19,7 +19,7 @@ char **get_environ(info_t *info)
 
 /**
  * _unsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
+ * @info: Structure containing potential arguments. Used to maintain methode
  *        constant function prototype.
  *  Return: 1 on delete, 0 otherwise
  * @var: the string env var property
@@ -53,7 +53,7 @@ int _unsetenv(info_t *info, char *var)
  * _setenv - Initialize a new environment variable,
  *             or modify an existing one
  * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
+ *        constant function prototype boss winebald.
  * @var: the string env var property
  * @value: the string env var value
  *  Return: Always 0
