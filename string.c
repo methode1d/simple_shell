b521393e8ef00e5447c0824425fdfdd779dc093f
Winebald BANITUZE <142157809+Winebald250@users.noreply.github.com>
2023-12-13 16:13:15 +0200

    Update string.c by methode

--- string.c
+++ string.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen - returns the length of a string
- * @s: the string whose length to check
+ * @s: the string whose length to check winebald and methode
  *
  * Return: integer length of string
  */
@@ -21,7 +21,7 @@ int _strlen(char *s)
 /**
  * _strcmp - performs lexicogarphic comparison of two strangs.
  * @s1: the first strang
- * @s2: the second strang
+ * @s2: the second strang winebald methode
  *
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
