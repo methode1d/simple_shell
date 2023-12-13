8017df9d1c3de3cccc3f18d7b8727c78f44da3c5
Winebald BANITUZE <142157809+Winebald250@users.noreply.github.com>
2023-12-13 16:12:36 +0200

    Update shell_loop.c by winebald

--- shell_loop.c
+++ shell_loop.c
@@ -2,10 +2,10 @@
 
 /**
  * hsh - main shell loop
- * @info: the parameter & return info struct
+ * @info: the parameter & return info struct methode duhujubumwe
  * @av: the argument vector from main()
  *
- * Return: 0 on success, 1 on error, or error code
+ * Return: 0 on success, 1 on error, or error code winebald
  */
 int hsh(info_t *info, char **av)
 {
